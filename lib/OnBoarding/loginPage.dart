62c93f119d30194d6d9714966f52fb177e4d1250
@2001devraj.c <2001devraj.c@gmail.com>
2024-06-26 23:22:15 -0400

    add login page ui

--- lib/OnBoarding/loginPage.dart
+++ lib/OnBoarding/loginPage.dart
@@ -1,26 +1,59 @@
 import 'package:flutter/material.dart';
-import 'package:hypeclip/Widgets/textField.dart';
+import 'package:hypeclip/Widgets/textField.dart'; // Ensure this custom widget supports `validator`
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   LoginPage({super.key});
 
-final usernameOrEmailController = TextEditingController();
-final passwordController = TextEditingController();
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>(); // Step 2
+  final usernameOrEmailController = TextEditingController();
+  final passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: Center(
-            child: Column(
-      children: [
-        const SizedBox(height: 160),
-        //const Icon(Icons.account_circle_outlined, size: 50),
-        const SizedBox(height: 30),
-        MyTextField(controller: usernameOrEmailController, hintText: 'Email or Username', obscureText: false, suffixIcon: Icon(Icons.email_outlined),),
-        SizedBox(height: 30),
-        MyTextField(controller: passwordController, hintText: 'Password', obscureText: true, suffixIcon: Icon(Icons.password_outlined),),
-        SizedBox(height: 15),
-         // forgot password?
+      child: Center(
+        child: Form(
+          // Step 3
+          key: _formKey,
+          child: Column(
+            children: [
+              const SizedBox(height: 160),
+              const SizedBox(height: 30),
+              MyTextField(
+                controller: usernameOrEmailController,
+                hintText: 'Email or Username',
+                obscureText: false,
+                suffixIcon: Icon(Icons.email_outlined),
+                validator: (value) {
+                  // Ensure your MyTextField supports this
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your email or username';
+                  }
+
+                  return null;
+                },
+              ),
+              SizedBox(height: 30),
+              MyTextField(
+                controller: passwordController,
+                hintText: 'Password',
+                obscureText: true,
+                suffixIcon: Icon(Icons.password_outlined),
+                validator: (value) {
+                  // Ensure your MyTextField supports this
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your password';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 15),
+              // Forgot password?
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 35 + 12),
                 child: Row(
@@ -28,32 +61,101 @@ final passwordController = TextEditingController();
                   children: [
                     Text(
                       'Forgot Password?',
-                      style: TextStyle(color: Colors.grey[600], fontSize: 14, decoration: TextDecoration.underline,decorationStyle: TextDecorationStyle.solid, // Specify the style of the underline
-    decorationThickness: 1.0),
+                      style: TextStyle(
+                        color: Color.fromARGB(255, 150, 150, 150),
+                        fontSize: 14,
+                        decoration: TextDecoration.underline,
+                        decorationStyle: TextDecorationStyle.solid,
+                        decorationThickness: 1.0,
+                      ),
                     ),
                   ],
                 ),
               ),
-        SizedBox(height: 30),
-        ElevatedButton(
-          
-          
-          onPressed: () {
-            // Validate the form
-            // If the form is valid, display a snackbar. In the real world,
-            // you'd often call a server or save the information in a database.
-            ScaffoldMessenger.of(context).showSnackBar(
-              const SnackBar(content: Text('Processing Data')),
-            );
-          },
-          child: const Text('Login'),
+              SizedBox(height: 30),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 35),
+                child: ElevatedButton(
+                  style: ButtonStyle(
+                    alignment: Alignment.center,
+                    minimumSize:
+                        WidgetStateProperty.all(Size(double.infinity, 57)),
+                    backgroundColor: WidgetStateProperty.all(
+                        Colors.black),
+                    foregroundColor: WidgetStateProperty.all(Color.fromARGB(255, 8, 104, 187)),
+                    shape: WidgetStateProperty.all(RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(24),
+                    )),
+                  ),
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      // Form is valid, proceed with the login
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(content: Text('Processing Data')),
+                      );
+                    }
+                  },
+                  child: Text('Login'),
+                ),
+              ),
+              SizedBox(height: 30),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 35),
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: Divider(
+                        color: Colors.white,
+                        thickness: 1,
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 10),
+                      child: Text('or',
+                          style: TextStyle(color: Colors.white, fontSize: 16)),
+                    ),
+                    Expanded(
+                      child: Divider(
+                        color: Colors.white,
+                        thickness: 1,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 30),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 35),
+                child: ElevatedButton(
+                  onPressed: () => {
+                    
+                  },
+                  style: ButtonStyle(
+                    backgroundColor: WidgetStateProperty.all(Color.fromRGBO(18, 18, 18, 1)),
+                    foregroundColor: WidgetStateProperty.all(Colors.white),
+                    side: WidgetStateProperty.all(BorderSide(color: Colors.white,)),
+                    minimumSize:
+                        WidgetStateProperty.all(Size(double.infinity, 57)),
+                    shape: WidgetStateProperty.all(RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(24),
+                    )),
+                  ),
+                  child: Stack(
+                    alignment: Alignment.center,
+                    children: <Widget>[
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Image.asset('assets/googleicondark.png'), // Google icon, change as needed
+                      ),
+                      Text('Continue with Google', style:  TextStyle(fontWeight: FontWeight.bold),),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
-
-          
-        
-      ],
-    )
-    )
+      ),
     );
   }
 }
--- lib/Widgets/textField.dart
+++ lib/Widgets/textField.dart
@@ -1,42 +1,73 @@
 import 'package:flutter/material.dart';
 
-class MyTextField extends StatelessWidget {
-  final controller;
+class MyTextField extends StatefulWidget {
+  final TextEditingController controller;
   final String hintText;
   final bool obscureText;
-  final Icon? suffixIcon; // Define the prefixIcon parameter
-
+  final Widget? suffixIcon;
+  final FormFieldValidator<String>? validator;
+// Added prefixIcon parameter
 
   const MyTextField({
     Key? key,
-    this.suffixIcon, // Add the prefixIcon parameter to the constructor
+    this.suffixIcon,
     required this.controller,
     required this.hintText,
-    required this.obscureText,
-  });
+    this.obscureText = true, 
+    this.validator, // Default to true, can be overridden
+  }) : super(key: key);
+
+  @override
+  _MyTextFieldState createState() => _MyTextFieldState();
+}
+
+class _MyTextFieldState extends State<MyTextField> {
+  late bool _isObscured;
+
+  @override
+  void initState() {
+    super.initState();
+    _isObscured = widget.obscureText;
+  }
+
+  void _togglePasswordVisibility() {
+    setState(() {
+      _isObscured = !_isObscured;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 35.0),
-      child: TextField(
-        controller: controller,
-        obscureText: obscureText,
+      child: TextFormField(
+        
+        validator: widget.validator,
+        controller: widget.controller,
+        obscureText: widget.obscureText ? _isObscured : false,
         decoration: InputDecoration(
-            enabledBorder: OutlineInputBorder(
-              borderSide: BorderSide(color: Color.fromARGB(255, 0, 0, 0)),
-              borderRadius: BorderRadius.all(Radius.circular(16))),
-            focusedBorder: OutlineInputBorder(
-                borderSide: BorderSide(color: Color.fromARGB(255, 255, 255, 255)),
-                borderRadius: BorderRadius.all(Radius.circular(16))),
-            fillColor: Color.fromRGBO(0,0,0,100),
-            filled: true,
-            hintText: hintText,
-            suffixIcon: suffixIcon,
-            
-            
-            hintStyle: TextStyle(color: Colors.grey[600], fontSize: 14),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Color.fromARGB(0, 69, 69, 69), width: 2),
+            borderRadius: BorderRadius.all(Radius.circular(14)),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Color.fromARGB(255, 255, 255, 255), width: 2),
+            borderRadius: BorderRadius.all(Radius.circular(14)),
+          ),
+          fillColor: Color.fromRGBO(0, 0, 0, 100),
+          filled: true,
+          hintText: widget.hintText,// Use the prefixIcon if provided
+          suffixIcon: widget.obscureText ? IconButton(
+            icon: Icon(
+              // Toggle the icon based on _isObscured
+              _isObscured ? Icons.visibility_off : Icons.visibility,
+              color: Color.fromARGB(255, 200, 200, 200),
+            ),
+            onPressed: _togglePasswordVisibility,
+          ) : widget.suffixIcon,
+          hintStyle: TextStyle(color: Color.fromARGB(255, 150, 150, 150), fontSize: 14),
+        ),
       ),
-    ));
+    );
   }
 }
